833843d1b0accb6c64d13f875543eeb199f7cde9
Bas Kleisen <baskleisen@gmail.com>
2021-12-09 15:54:33 +0100

    another curl

--- post.sh
+++ post.sh
@@ -1,2 +1,3 @@
 # curl -X POST -F 'sql=select * from film' localhost:5000/dvdrental/
-curl -X POST -H "Content-Type: application/json"  --data '{"sql":"select * from film"}' localhost:5000/dvdrental/
+# curl -X POST -H "Content-Type: application/json"  --data '{"sql":"select * from film"}' localhost:5000/dvdrental/
+curl -X POST -H "Content-Type: application/json"  --data '{"sql":"select * from film"}' -L https://a7d1-195-78-54-226.ngrok.io/dvdrental/
